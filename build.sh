2ee77174159c0ca1e19d7d197f2cc05910d8f6a9
pavithra <124659114+Pavithra-1726@users.noreply.github.com>
2025-01-30 00:08:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 # Variables
 IMAGE_NAME="react-app"
 DOCKER_HUB_USERNAME="bubbly17"
+DEV_TAG="$DOCKER_HUB_USERNAME/dev:$IMAGE_NAME"
 
 # Build Docker Image
 docker build -t $IMAGE_NAME .
 
-# Tag the image
-docker tag $IMAGE_NAME $DOCKER_HUB_USERNAME/dev:$IMAGE_NAME
+# Tag the Image for Docker Hub
+docker tag $IMAGE_NAME $DEV_TAG
 
-echo "Docker Image built successfully."
-
+echo "Docker Image built and tagged successfully."
