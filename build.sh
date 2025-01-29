716960f091e0aa26f3e6fab58df976df4a091dcf
Pavithra1726 <pavi.honey014@gmail.com>
2025-01-29 23:54:15 +0530

    first commit after adding all files

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-# Build Docker image
-docker build -t react-app:latest .
+
+# Variables
+IMAGE_NAME="react-app"
+DOCKER_HUB_USERNAME="bubbly17"
+
+# Build Docker Image
+docker build -t $IMAGE_NAME .
+
+# Tag the image
+docker tag $IMAGE_NAME $DOCKER_HUB_USERNAME/dev:$IMAGE_NAME
+
+echo "Docker Image built successfully."
 
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-docker build -t react-app:latest .
+
+# Variables
+IMAGE_NAME="react-app"
+DOCKER_HUB_USERNAME="bubbly17"
+
+# Pull the latest image
+docker pull $DOCKER_HUB_USERNAME/dev:$IMAGE_NAME
+
+# Stop & remove old container
+docker stop $IMAGE_NAME
+docker rm $IMAGE_NAME
+
+# Run new container
+docker run -d -p 80:80 --name $IMAGE_NAME $DOCKER_HUB_USERNAME/dev:$IMAGE_NAME
+
+echo "Application deployed successfully."
 
